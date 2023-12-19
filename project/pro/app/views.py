f13e73a03a2a5eee4fe051a0516a0fe57228c317
anandbkrishna1 <140711267+anandbkrishna1@users.noreply.github.com>
2023-12-19 04:28:04 -0800

    Add files via upload

--- project/pro/app/views.py
+++ project/pro/app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from app.form import*
 # Create your views here.
 
 
 
 def home(request):
     return render(request,'index.html')
+
 
