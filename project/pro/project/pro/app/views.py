dcb1a35e0aa27686687229c98ada212e0abefe27
anandbkrishna1 <140711267+anandbkrishna1@users.noreply.github.com>
2023-12-19 04:25:33 -0800

    Add files via upload

--- project/pro/project/pro/app/views.py
+++ project/pro/project/pro/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from app.form import*
 # Create your views here.
 
 
@@ -8,25 +8,4 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'index.html')
 
-def signup(request):
-    if request.method=='POST':
-        if request.method=='POST':
-            username=request.POST.get('user_name')
-            email= request.POST.get('email')
-            pwd = request.POST.get('pwd')
-            cnfpwd = request.POST.get('cnfpwd')
-            if pwd==cnfpwd:
-                if User.objects.filter(username=username).exists():
-                    messages.info(request,"Username Already Exist")
-                    return redirect('/register')
-                elif User.objects.filter(email=email).exists():
-                    messages.info(request, "Email Already Exist")
-                    return redirect('/register')
-                else:
-                    user=User.objects.create(username=username,email=email,password=pwd)
-                    user.save()
-                return redirect('/')
-            else:
-                messages.info(request, "Password Not Match")
-                return redirect('/register')
-    return render(request,'signup.html')
+
